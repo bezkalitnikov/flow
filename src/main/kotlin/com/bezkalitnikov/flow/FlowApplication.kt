4ea1a43069a2da724a1123cfcf7949611639a8f6
Aleksei Smirnov <apsmirnov@almworks.com>
2022-12-09 12:26:18 +0100

    Created project from the scratch * Kotlin + Reactive Web + R2DBC

---
+++ src/main/kotlin/com/bezkalitnikov/flow/FlowApplication.kt
@@ -0,0 +1,11 @@
+package com.bezkalitnikov.flow
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FlowApplication
+
+fun main(args: Array<String>) {
+	runApplication<FlowApplication>(*args)
+}
---
+++ src/test/kotlin/com/bezkalitnikov/flow/FlowApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.bezkalitnikov.flow
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class FlowApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
