ec2fe255b846375517d6ada977e00851bb8153e7
Aleksei Smirnov <apsmirnov@almworks.com>
2022-12-12 20:33:56 +0100

    Added configuration for react

---
+++ frontend/webpack.config.js
@@ -0,0 +1,39 @@
+const path = require("path");
+const HTMLWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  resolve: {
+    modules: [
+      path.resolve(__dirname, 'src'),
+      'node_modules'
+    ]
+  },
+  entry: "index.js",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve("build"),
+    publicPath: "/",
+  },
+  module: {
+    rules:[
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: "babel-loader"
+      },
+      {
+        test: /\.html$/,
+        use: "html-loader"
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      }
+    ],
+  },
+  plugins: [
+    new HTMLWebpackPlugin({
+      template: "src/index.html"
+    }),
+  ]
+}
