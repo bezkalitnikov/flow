571030d2e449a70abb448ff8417242d46216aaa7
Aleksei Smirnov <apsmirnov@almworks.com>
2022-12-14 13:37:50 +0100

    Adjusted IndexRouter

--- backend/src/main/kotlin/com/bezkalitnikov/flow/IndexRouter.kt
+++ backend/src/main/kotlin/com/bezkalitnikov/flow/IndexRouter.kt
@@ -1,23 +1,20 @@
 package com.bezkalitnikov.flow
 
-import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.core.io.Resource
 import org.springframework.http.MediaType
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.reactive.function.server.RequestPredicates.GET
 import org.springframework.web.reactive.function.server.RouterFunction
 import org.springframework.web.reactive.function.server.RouterFunctions
-
-import org.springframework.web.reactive.function.server.RequestPredicates.GET
 import org.springframework.web.reactive.function.server.ServerResponse
 import org.springframework.web.reactive.function.server.ServerResponse.ok
 
 @Configuration(proxyBeanMethods = false)
+@RequestMapping("/api/v1/")
 class IndexRouter {
   @Bean
-  fun indexRoute(
-    @Value("classpath:static/index.html")
-    html: Resource): RouterFunction<ServerResponse> {
-    return RouterFunctions.route(GET("/")) { ok().contentType(MediaType.TEXT_HTML).bodyValue(html) }
+  fun indexRoute(): RouterFunction<ServerResponse> {
+    return RouterFunctions.route(GET("/")) { ok().contentType(MediaType.APPLICATION_JSON).bodyValue("{status:ok}") }
   }
 }
