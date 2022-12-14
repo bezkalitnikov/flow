2b152fd29283ebfe14557b44f4a65196fb2d0977
Aleksei Smirnov <apsmirnov@almworks.com>
2022-12-14 15:11:46 +0100

    Reverted from jsx to js extension

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
-import App from "./App.jsx";
+import App from "./App.js";
 import "./styles.css";
 
 const appRouting = (
